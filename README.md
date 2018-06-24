1c779027bcd85cc5075fad475ffba81cff2febfb
Victor Hugo Marques <vmarques.dev@gmail.com>
2018-06-23 22:54:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# VMarquesdev.com.br
+Powered by [GatsbyJS](https://gatsbyjs.org)
+
+To run this locally, clone this repo and run
+```
+$ yarn
+$ gatsby develop
+```
---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: 'VMarquesdev.com.br',
+    author: 'Victor Hugo Marques',
+    description: 'Full-Stack Developer',
+    siteUrl: 'http://vmarquesdev.com.br'
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
